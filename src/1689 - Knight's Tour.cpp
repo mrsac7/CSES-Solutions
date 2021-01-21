2c948525a500e4e3e8b5607552e4bbc63faf3ce4
Sachin Srivastava <mrsac7@gmail.com>
2021-01-21 22:30:26 +0530

    Update 1689 - Knight's Tour.cpp

--- src/1689 - Knight's Tour.cpp
+++ src/1689 - Knight's Tour.cpp
@@ -52,7 +52,7 @@ int deg(int x1, int y1){
     int s = 0;
     rep(i1,0,8){
         int x2 = x1+fx[i1], y2 = y1+fy[i1];
-        if (x2>=1 && x2<=8 && y2>=1 && y2<=8 && !grid[x1][y1]) s++;
+        if (x2>=1 && x2<=8 && y2>=1 && y2<=8 && !grid[x2][y2]) s++;
     }
     return s;
 }
